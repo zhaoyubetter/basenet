6e4ca041ebaf1a4521db38d79839dbcb14b9be69
zhaoyu57 <zhaoyu1@jd.com>
2017-03-10 10:02:06 +0800

    添加 volley 例子代码

--- app/src/main/java/basenet/better/basenet/MainActivity.java
+++ app/src/main/java/basenet/better/basenet/MainActivity.java
@@ -1,13 +1,34 @@
 package basenet.better.basenet;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import okhttp3.OkHttpClient;
 
 public class MainActivity extends AppCompatActivity {
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-	}
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setTitle("基础网络测试");
+
+        findViewById(R.id.btn_volley).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), VolleyActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        findViewById(R.id.btn_okhttp).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), OkHttpActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
 }
---
+++ app/src/main/java/basenet/better/basenet/VolleyActivity.java
@@ -0,0 +1,70 @@
+package basenet.better.basenet;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.text.method.ScrollingMovementMethod;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import lib.basenet.request.AbsRequest;
+import lib.basenet.request.AbsRequestCallBack;
+import lib.basenet.volley.VolleyRequest;
+
+public class VolleyActivity extends AppCompatActivity implements View.OnClickListener {
+
+    EditText url;
+    EditText timeout;
+
+    Button get;
+    Button post;
+
+    TextView result;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_volley);
+
+        url = (EditText) findViewById(R.id.url);
+        timeout = (EditText) findViewById(R.id.timeout);
+        get = (Button) findViewById(R.id.get);
+        post = (Button) findViewById(R.id.post);
+        result = (TextView) findViewById(R.id.result);
+        result.setMovementMethod(ScrollingMovementMethod.getInstance());
+
+        get.setOnClickListener(this);
+        post.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        result.setText("");
+        long tTimeout = Integer.parseInt(timeout.getText().toString()) * 1000;
+        String tUrl = url.getText().toString();
+
+        final AbsRequest.Builder builder = new VolleyRequest.Builder(getApplicationContext())
+                .timeout(tTimeout)
+                .url(tUrl)
+                .callback(new AbsRequestCallBack() {
+                    @Override
+                    public void onSuccess(Object o) {
+                        super.onSuccess(o);
+                        result.setText(o.toString());
+                    }
+
+                    @Override
+                    public void onFailure(Throwable e) {
+                        super.onFailure(e);
+                        result.setText(e.toString());
+                    }
+                });
+
+        if (R.id.get == v.getId()) {
+            builder.type(AbsRequest.RequestType.GET).build().request();
+        } else if (R.id.post == v.getId()) {
+            builder.type(AbsRequest.RequestType.POST).build().request();
+        }
+    }
+}
