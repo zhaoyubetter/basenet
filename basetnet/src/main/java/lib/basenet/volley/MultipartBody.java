babfb398054bbb1c7d6cd395024a7a088de9e216
zhaoyubetter@126.com <zhaoyubetter@126.com>
2017-03-12 23:01:01 +0800

    增加新类，让 volley 支持小文件的上传

---
+++ basetnet/src/main/java/lib/basenet/volley/MultipartBody.java
@@ -0,0 +1,211 @@
+package lib.basenet.volley;
+
+
+import android.text.TextUtils;
+
+import java.io.ByteArrayOutputStream;
+import java.io.Closeable;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.Random;
+
+
+/**
+ * 让 volley 支持文件上传
+ * 参考：http://blog.csdn.net/bboyfeiyu/article/details/42266869
+ * Created by zhaoyu1 on 2017/3/10.
+ */
+public class MultipartBody {
+
+
+	private final static char[] MULTIPART_CHARS = "-_1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ".toCharArray();
+
+	private final String CONTENT_TYPE = "Content-Type: ";
+	private final String CONTENT_DISPOSITION = "Content-Disposition: ";
+	/**
+	 * 文本参数和字符集
+	 */
+	private final String TYPE_TEXT_CHARSET = "text/plain; charset=UTF-8";
+
+	/**
+	 * 字节流参数
+	 */
+	private final String TYPE_OCTET_STREAM = "application/octet-stream";
+	/**
+	 * 二进制参数
+	 */
+	private final byte[] BINARY_ENCODING = "Content-Transfer-Encoding: binary\r\n\r\n".getBytes();
+	/**
+	 * 文本参数
+	 */
+	private final byte[] BIT_ENCODING = "Content-Transfer-Encoding: 8bit\r\n\r\n".getBytes();
+
+	/**
+	 * 换行符
+	 */
+	private final String NEW_LINE_STR = "\r\n";
+
+	/**
+	 * 分隔符
+	 */
+	private String mBoundary = null;
+	/**
+	 * 输出流
+	 */
+	ByteArrayOutputStream mOutputStream = new ByteArrayOutputStream();
+
+
+	private StringBuilder mSb = new StringBuilder();
+
+
+	public MultipartBody() {
+		this.mBoundary = generateBoundary();
+	}
+
+	/**
+	 * 生成分隔符
+	 *
+	 * @return
+	 */
+	private String generateBoundary() {
+		final StringBuffer buf = new StringBuffer();
+		final Random rand = new Random();
+		for (int i = 0; i < 30; i++) {
+			buf.append(MULTIPART_CHARS[rand.nextInt(MULTIPART_CHARS.length)]);
+		}
+		return buf.toString();
+	}
+
+	/**
+	 * 开头分隔符
+	 * --WebKitFormBoundaryMcD0BA59mk3aFx4I
+	 */
+	private void writeFirstBoundary() throws IOException {
+		mOutputStream.write(("--" + mBoundary + NEW_LINE_STR).getBytes());
+		mSb.append(("--" + mBoundary + NEW_LINE_STR));
+	}
+
+	/**
+	 * 添加文本参数
+	 */
+	public void addStringPart(String paramName, String paramValue) {
+		// writeToOutputStream(paramName, paramValue.getBytes(), TYPE_TEXT_CHARSET, BIT_ENCODING, "");
+		writeToOutputStream(paramName, paramValue.getBytes(), null, null, "");
+	}
+
+	/**
+	 * 添加二进制参数, 例如Bitmap的字节流参数
+	 *
+	 * @param paramName
+	 * @param rawData
+	 */
+	public void addBinaryPart(String paramName, final byte[] rawData) {
+		writeToOutputStream(paramName, rawData, TYPE_OCTET_STREAM, BINARY_ENCODING, "no-file");
+	}
+
+	/**
+	 * 添加文件参数,可以实现文件上传功能
+	 *
+	 * @param key
+	 * @param file
+	 */
+	public void addFilePart(final String key, final File file) {
+		InputStream fin = null;
+		try {
+			fin = new FileInputStream(file);
+			writeFirstBoundary();
+			mOutputStream.write(getContentDispositionBytes(key, file.getName()));
+			final String type = CONTENT_TYPE + TYPE_OCTET_STREAM + NEW_LINE_STR;
+			mOutputStream.write(type.getBytes());
+			mOutputStream.write(NEW_LINE_STR.getBytes());
+
+			final byte[] tmp = new byte[4096];
+			int len = 0;
+			while ((len = fin.read(tmp)) != -1) {
+				mOutputStream.write(tmp, 0, len);
+			}
+			mOutputStream.write(NEW_LINE_STR.getBytes());
+			mOutputStream.flush();
+		} catch (final IOException e) {
+			e.printStackTrace();
+		} finally {
+			closeSilently(fin);
+		}
+	}
+
+
+	/**
+	 * 将数据写入到输出流中
+	 *
+	 * @param paramName
+	 * @param rawData
+	 * @param type
+	 * @param encodingBytes
+	 * @param fileName
+	 */
+	private void writeToOutputStream(String paramName,
+									 byte[] rawData,
+									 String type,
+									 byte[] encodingBytes,
+									 String fileName) {
+		try {
+			writeFirstBoundary();
+			if(type != null && type.length() > 0) {
+				mOutputStream.write((CONTENT_TYPE + type + NEW_LINE_STR).getBytes());
+			}
+			mOutputStream.write(getContentDispositionBytes(paramName, fileName));
+			mOutputStream.write(NEW_LINE_STR.getBytes());
+			mSb.append(new String(rawData));
+
+			if(encodingBytes != null) {
+				mOutputStream.write(encodingBytes);
+			}
+
+			mOutputStream.write(rawData);
+			mOutputStream.write(NEW_LINE_STR.getBytes());
+		} catch (final IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private byte[] getContentDispositionBytes(String paramName, String fileName) {
+		StringBuilder stringBuilder = new StringBuilder();
+		stringBuilder.append(CONTENT_DISPOSITION + "form-data; name=\"" + paramName + "\"");
+		if (!TextUtils.isEmpty(fileName)) {
+			stringBuilder.append("; filename=\"" + fileName + "\"");
+		}
+		return stringBuilder.append(NEW_LINE_STR).toString().getBytes();
+	}
+
+
+	public long getContentLength() {
+		return mOutputStream.toByteArray().length;
+	}
+
+	// Content-Type: multipart/form-data; boundary=
+	public String getContentType() {
+		return "multipart/form-data; boundary=" + mBoundary;
+	}
+
+	public void writeTo(final OutputStream outstream) throws IOException {
+		// 参数最末尾的结束符
+		final String endString = "--" + mBoundary + "--" + NEW_LINE_STR;
+		// 写入结束符
+		mOutputStream.write(endString.getBytes());
+		outstream.write(mOutputStream.toByteArray());
+	}
+
+
+	private void closeSilently(Closeable closeable) {
+		try {
+			if (closeable != null) {
+				closeable.close();
+			}
+		} catch (final IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
--- basetnet/src/main/java/lib/basenet/volley/VolleyRequest.java
+++ basetnet/src/main/java/lib/basenet/volley/VolleyRequest.java
@@ -12,6 +12,7 @@ import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
+import java.io.File;
 import java.util.Map;
 
 import lib.basenet.request.AbsRequest;
@@ -27,109 +28,163 @@ import lib.basenet.request.AbsRequest;
  */
 public class VolleyRequest extends AbsRequest {
 
-    private static RequestQueue requestQueue;
-    private Request mRequest;
+	private static RequestQueue requestQueue;
+	private Request mRequest;
 
-    private VolleyRequest(Builder builder) {
-        super(builder);
-        if (requestQueue == null) {
-            synchronized (VolleyRequest.class) {
-                if (requestQueue == null) {
-                    requestQueue = Volley.newRequestQueue(builder.mCtx);
-                }
-            }
-        }
-    }
+	private VolleyRequest(Builder builder) {
+		super(builder);
+		if (requestQueue == null) {
+			synchronized (VolleyRequest.class) {
+				if (requestQueue == null) {
+					requestQueue = Volley.newRequestQueue(builder.mCtx);
+				}
+			}
+		}
+	}
 
-    @Override
-    protected void get() {
-        realRequest(Request.Method.GET);
-    }
+	@Override
+	protected void get() {
+		realRequest(Request.Method.GET);
+	}
 
-    @Override
-    protected void post() {
-        realRequest(Request.Method.POST);
-    }
+	@Override
+	protected void post() {
+		realRequest(Request.Method.POST);
+	}
 
-    private void realRequest(final int reqType) {
-        int tReqType = Request.Method.GET;
-        String tUrl = mUrl;
-        switch (reqType) {
-            case Request.Method.GET:
-                tReqType = Request.Method.GET;
-                tUrl = generateUrl(mUrl, mParams);
-                break;
-            case Request.Method.POST:
-                tReqType = Request.Method.POST;
-                break;
-        }
+	private void realRequest(final int reqType) {
+		int tReqType = Request.Method.GET;
+		String tUrl = mUrl;
+		switch (reqType) {
+			case Request.Method.GET:
+				tReqType = Request.Method.GET;
+				tUrl = generateUrl(mUrl, mParams);
+				break;
+			case Request.Method.POST:
+				tReqType = Request.Method.POST;
+				break;
+		}
 
-        mRequest = new StringRequest(tReqType, tUrl, new Response.Listener<String>() {
-            @Override
-            public void onResponse(String response) {
-                if (mCallBack != null) {
-                    mCallBack.onSuccess(response);
-                }
-            }
-        }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                if (mCallBack != null) {
-                    mCallBack.onFailure(error);
-                }
-            }
-        }) {
-            @Override
-            public Map<String, String> getHeaders() throws AuthFailureError {
-                Map<String, String> superHeader = super.getHeaders();
-                if (mHeader != null && mHeader.size() > 0) {
-                    superHeader = mHeader;
-                }
-                return superHeader;
-            }
+		// 如果是上传文件
+		if (mUploadFiles != null && mUploadFiles.size() > 0) {
+			upload();
+			return;
+		}
 
-            // 设置Body参数
-            @Override
-            protected Map<String, String> getParams() throws AuthFailureError {
-                Map<String, String> tParams = super.getParams();
-                if (mParams != null && mParams.size() > 0 && reqType == Request.Method.POST) {
-                    tParams = mParams;
-                }
-                return tParams;
-            }
-        };
+		mRequest = new StringRequest(tReqType, tUrl, new Response.Listener<String>() {
+			@Override
+			public void onResponse(String response) {
+				if (mCallBack != null) {
+					mCallBack.onSuccess(response);
+				}
+			}
+		}, new Response.ErrorListener() {
+			@Override
+			public void onErrorResponse(VolleyError error) {
+				if (mCallBack != null) {
+					mCallBack.onFailure(error);
+				}
+			}
+		}) {
+			@Override
+			public Map<String, String> getHeaders() throws AuthFailureError {
+				Map<String, String> superHeader = super.getHeaders();
+				if (mHeader != null && mHeader.size() > 0) {
+					superHeader = mHeader;
+				}
+				return superHeader;
+			}
 
-        // 设置此次请求超时时间
-        if (mTimeOut > 1000) {
-            mRequest.setRetryPolicy(new DefaultRetryPolicy((int) mTimeOut, 0, DefaultRetryPolicy.DEFAULT_BACKOFF_MULT));
-        } else {
-            mRequest.setRetryPolicy(new DefaultRetryPolicy((int) DEFAULT_TIME_OUT, 0, DefaultRetryPolicy.DEFAULT_BACKOFF_MULT));
-        }
-        mRequest.setTag(mTag);
-        requestQueue.add(mRequest);
-    }
+			// 设置Body参数
+			@Override
+			protected Map<String, String> getParams() throws AuthFailureError {
+				Map<String, String> tParams = super.getParams();
+				if (mParams != null && mParams.size() > 0 && reqType == Request.Method.POST) {
+					tParams = mParams;
+				}
+				return tParams;
+			}
+		};
 
-    @Override
-    public void cancel() {
-        if (mRequest != null) {
-            mRequest.cancel();
-        } else if (mTag != null) {
-            requestQueue.cancelAll(mTag);
-        }
-    }
+		// 设置此次请求超时时间
+		if (mTimeOut > 1000) {
+			mRequest.setRetryPolicy(new DefaultRetryPolicy((int) mTimeOut, 0, DefaultRetryPolicy.DEFAULT_BACKOFF_MULT));
+		} else {
+			mRequest.setRetryPolicy(new DefaultRetryPolicy((int) DEFAULT_TIME_OUT, 0, DefaultRetryPolicy.DEFAULT_BACKOFF_MULT));
+		}
+		mRequest.setTag(mTag);
+		requestQueue.add(mRequest);
+	}
+
+	/**
+	 * 文件上传
+	 */
+	private void upload() {
+		mRequest = new PostUploadRequest(Request.Method.POST, mUrl, new Response.ErrorListener() {
+			@Override
+			public void onErrorResponse(VolleyError error) {
+				if (mCallBack != null) {
+					mCallBack.onFailure(error);
+				}
+			}
+		}, new Response.Listener<String>() {
+			@Override
+			public void onResponse(String response) {
+				if (mCallBack != null) {
+					mCallBack.onSuccess(response);
+				}
+			}
+		}) {
+			@Override
+			public Map<String, String> getHeaders() throws AuthFailureError {
+				Map<String, String> superHeader = super.getHeaders();
+				if (mHeader != null && mHeader.size() > 0) {
+					superHeader = mHeader;
+				}
+				return superHeader;
+			}
+
+			// 设置Body参数
+			@Override
+			protected Map<String, String> getParams() throws AuthFailureError {
+				Map<String, String> tParams = super.getParams();
+				if (mParams != null && mParams.size() > 0) {
+					tParams = mParams;
+				}
+				return tParams;
+			}
+
+			@Override
+			public Map<String, File> getUploadFiles() {
+				return mUploadFiles;
+			}
+		};
+
+		mRequest.setTag(mTag);
+		requestQueue.add(mRequest);
+	}
+
+	@Override
+	public void cancel() {
+		if (mRequest != null) {
+			mRequest.cancel();
+		} else if (mTag != null) {
+			requestQueue.cancelAll(mTag);
+		}
+	}
 
 
-    public static class Builder extends AbsRequest.Builder {
+	public static class Builder extends AbsRequest.Builder {
 
-        private Context mCtx;
+		private Context mCtx;
 
-        public Builder(Context ctx) {
-            this.mCtx = ctx;
-        }
+		public Builder(Context ctx) {
+			this.mCtx = ctx;
+		}
 
-        @Override
-        public AbsRequest build() {
-            return new VolleyRequest(this);
-        }
-    }
+		@Override
+		public AbsRequest build() {
+			return new VolleyRequest(this);
+		}
+	}
 }
