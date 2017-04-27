52b0a4c263ba529aa8af0e8f9e0e11ae30e4b5e9
zhaoyubetter@126.com <zhaoyubetter@126.com>
2017-04-27 11:21:52 +0800

    加入缓存拦截器

--- basetnet/src/main/java/lib/basenet/okhttp/cache/NetCacheInterceptor.java
+++ basetnet/src/main/java/lib/basenet/okhttp/cache/NetCacheInterceptor.java
@@ -2,9 +2,13 @@ package lib.basenet.okhttp.cache;
 
 import java.io.IOException;
 
+import lib.basenet.config.NetConfig;
+import lib.basenet.utils.NetworkUtils;
+import okhttp3.CacheControl;
 import okhttp3.Interceptor;
 import okhttp3.Request;
 import okhttp3.Response;
+
 
 /**
  * 缓存拦截
@@ -14,20 +18,52 @@ public class NetCacheInterceptor implements Interceptor {
 
 	@Override
 	public Response intercept(Chain chain) throws IOException {
-		final Request request = chain.request();
+		Request request = chain.request();
 
-		// 获取设置的缓存策略,不管 GET 还是 POST
-		if (null != request.cacheControl() && !request.cacheControl().noCache()) {
-			int maxAge = request.cacheControl().maxAgeSeconds();
-			final Response response = chain.proceed(request);
-			final Response wrapper = response.newBuilder()
-					.removeHeader("Pragma")
-					.removeHeader("Cache-control")
-					.header("Cache-control", "max-age=" + maxAge)
-					.build();
-			return wrapper;
-		}
+		// 从request获取是否能存储缓存
+		if (null != request.cacheControl() && !request.cacheControl().noStore()) {
+			// 是否有网络
+			boolean hasNet = NetworkUtils.hasNet(NetConfig.application);
+			// 原始响应
+			final Response originResponse;
+
+			if (!hasNet) {
+				// 无网络，强制从缓存中获取
+				request = request.newBuilder().cacheControl(CacheControl.FORCE_CACHE).build();
+				originResponse = chain.proceed(request);
+				return originResponse;
+			} else {
+				// 有网络，设置缓存时间 （通过下面一行，可拿到网络上的返回数据）
+				originResponse = chain.proceed(request);
+				// 再包装一下originResponse，方便后面的 CacheInterceptor、PostCacheInterceptor 来做缓存处理
+				return getWrapperResponse(request, originResponse);
+			}
+		}   // end 能存储缓存
 
 		return chain.proceed(request);
+	}
+
+	/**
+	 * 包装一下originResponse，方便后面的 CacheInterceptor 来做缓存处理
+	 *
+	 * @param request
+	 * @param originResponse
+	 * @return
+	 */
+	private Response getWrapperResponse(Request request, Response originResponse) {
+		int maxAge = request.cacheControl().maxAgeSeconds();
+		return originResponse.newBuilder()
+				.removeHeader("Pragma")
+				.removeHeader("Cache-control")
+				.header("Cache-control", "public, max-age=" + maxAge)
+				.build();
+	}
+
+
+	// 以下摘自 okhttp中的CacheInterceptor
+	private static Response stripBody(Response response) {
+		return response != null && response.body() != null
+				? response.newBuilder().body(null).build()
+				: response;
 	}
 }
