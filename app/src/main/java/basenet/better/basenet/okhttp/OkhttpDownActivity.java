0997d559d7c7be25a23dcd4013c3de8ff2f21382
zhaoyubetter@126.com <zhaoyubetter@126.com>
2018-03-04 23:44:46 +0800

    修复 okhttp3.6 版本 （get 下载时，设置失效问题）（需升级到 okhttp 3.10.0）版本；因项目涉及的东西，太多 暂时不升级吧

--- app/src/main/java/basenet/better/basenet/okhttp/OkhttpDownActivity.java
+++ app/src/main/java/basenet/better/basenet/okhttp/OkhttpDownActivity.java
@@ -26,180 +26,181 @@ import lib.basenet.response.Response;
 
 public class OkhttpDownActivity extends AppCompatActivity implements View.OnClickListener {
 
-    final String TAG = "down";
+	final String TAG = "down";
 
-    EditText et_down_url;
-    ProgressBar progress;
-    TextView progressTV;
-    TextView error;
+	EditText et_down_url;
+	ProgressBar progress;
+	TextView progressTV;
+	TextView error;
 
-    AbsRequest request;
+	AbsRequest request;
 
-    final String downUrl = "http://111.231.206.52:8080/yu/hehe.jpg";
+	final String downUrl = "http://111.231.206.52:8080/yu/hehe.jpg";
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_okhttp_down);
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_okhttp_down);
 
-        et_down_url = (EditText) findViewById(R.id.et_down_url);
-        progress = (ProgressBar) findViewById(R.id.progress);
-        progressTV = (TextView) findViewById(R.id.progressTV);
-        error = (TextView) findViewById(R.id.error);
-        progress.setMax(100);
+		et_down_url = (EditText) findViewById(R.id.et_down_url);
+		progress = (ProgressBar) findViewById(R.id.progress);
+		progressTV = (TextView) findViewById(R.id.progressTV);
+		error = (TextView) findViewById(R.id.error);
+		progress.setMax(100);
 
-        et_down_url.setText(downUrl);
+		et_down_url.setText(downUrl);
 
-        findViewById(R.id.down).setOnClickListener(this);
-        findViewById(R.id.cancel).setOnClickListener(this);
-        findViewById(R.id.pause).setOnClickListener(this);
-        findViewById(R.id.down_continue_).setOnClickListener(this);
-        findViewById(R.id.re_startDown).setOnClickListener(this);
-    }
+		findViewById(R.id.down).setOnClickListener(this);
+		findViewById(R.id.cancel).setOnClickListener(this);
+		findViewById(R.id.pause).setOnClickListener(this);
+		findViewById(R.id.down_continue_).setOnClickListener(this);
+		findViewById(R.id.re_startDown).setOnClickListener(this);
+	}
 
 
-    private void down() {
-        PermissionUtils.checkOnePermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE, "权限申请", new Runnable() {
-            @Override
-            public void run() {
-                realDown();
-            }
-        });
-    }
+	private void down() {
+		PermissionUtils.checkOnePermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE, "权限申请", new Runnable() {
+			@Override
+			public void run() {
+				realDown();
+			}
+		});
+	}
 
-    private void realDown() {
-        error.setText("");
-        Map<String, File> uploads = new HashMap<>();
-        final String absolutePath = Environment.getExternalStorageDirectory().getAbsolutePath();
-        File file = new File(absolutePath + "/" + "headfirst.apk");
-        request = new OkHttpRequest.Builder().url(et_down_url.getText().toString())
-                .downFile(file)
-                .callback(new AbsRequestCallBack() {
-                    @Override
-                    public void onSuccess(Response response) {
-                        super.onSuccess(response);
-                    }
+	private void realDown() {
+		error.setText("");
+		Map<String, File> uploads = new HashMap<>();
+		final String absolutePath = Environment.getExternalStorageDirectory().getAbsolutePath();
+		File file = new File(absolutePath + "/" + "headfirst.apk");
+		request = new OkHttpRequest.Builder().url(et_down_url.getText().toString())
+				.downFile(file)
+				.callback(new AbsRequestCallBack() {
+					@Override
+					public void onSuccess(Response response) {
+						super.onSuccess(response);
+					}
 
-                    @Override
-                    public void onFailure(final Throwable e) {
-                        super.onFailure(e);
-                        runOnUiThread(new Runnable() {
-                            @Override
-                            public void run() {
-                                error.setText(e.toString());
-                            }
-                        });
-                    }
+					@Override
+					public void onFailure(final Throwable e) {
+						super.onFailure(e);
+						runOnUiThread(new Runnable() {
+							@Override
+							public void run() {
+								error.setText(e.toString());
+							}
+						});
+					}
 
-                    @Override
-                    public void onProgressUpdate(final long contentLength, final long bytesRead, final boolean done) {
-                        super.onProgressUpdate(contentLength, bytesRead, done);
-                        runOnUiThread(new Runnable() {
-                            @Override
-                            public void run() {
-                                progress.setProgress((int) (bytesRead * 1.0f / contentLength * 100));
-                                progressTV.setText(contentLength + "/" + bytesRead);
-                            }
-                        });
-                    }
-                }).tag(TAG)
-                .build();
-        request.request();
-    }
+					@Override
+					public void onProgressUpdate(final long contentLength, final long bytesRead, final boolean done) {
+						super.onProgressUpdate(contentLength, bytesRead, done);
+						runOnUiThread(new Runnable() {
+							@Override
+							public void run() {
+								progress.setProgress((int) (bytesRead * 1.0f / contentLength * 100));
+								progressTV.setText(contentLength + "/" + bytesRead);
+							}
+						});
+					}
+				}).tag(TAG)
+				.build();
+		request.request();
+	}
 
-    @Override
-    public void onClick(View v) {
-        switch (v.getId()) {
-            case R.id.down:
-                PermissionUtils.checkOnePermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE, "权限申请", new Runnable() {
-                    @Override
-                    public void run() {
+	@Override
+	public void onClick(View v) {
+		switch (v.getId()) {
+			case R.id.down:
+				PermissionUtils.checkOnePermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE, "权限申请", new Runnable() {
+					@Override
+					public void run() {
 //				down();
-                        downPart();
-                    }
-                });
-                break;
-            case R.id.cancel:
-                NetUtils.getInstance().cancel(TAG);
-                break;
-            case R.id.pause:
-                if (downloadFileManager != null) {
-                    downloadFileManager.stopDownload();
-                }
-                break;
-            case R.id.down_continue_:
-                if (downloadFileManager != null) {
-                    downloadFileManager.startDownload();
-                }
-                break;
-            case R.id.re_startDown:
-                downloadFileManager.deleteDownload();
-                if (downloadFileManager != null) {
-                    downloadFileManager.startDownload();
-                }
-                break;
-        }
-    }
+						downPart();
+					}
+				});
+				break;
+			case R.id.cancel:
+				NetUtils.getInstance().cancel(TAG);
+				break;
+			case R.id.pause:
+				if (downloadFileManager != null) {
+					downloadFileManager.stopDownload();
+				}
+				break;
+			case R.id.down_continue_:
+				if (downloadFileManager != null) {
+					downloadFileManager.startDownload();
+				}
+				break;
+			case R.id.re_startDown:
+				if (downloadFileManager != null) {
+					downloadFileManager.deleteDownload();
+					downloadFileManager.startDownload();
 
-    private DownloadFileManager downloadFileManager;
+				}
+				break;
+		}
+	}
 
-    private void downPart() {
-        if(downloadFileManager != null && downloadFileManager.getStatus() == DownloadFileInfo.DOWNLOADING ) {
-            return;
-        }
+	private DownloadFileManager downloadFileManager;
 
-        DownloadFileInfo downloadFileInfo = new DownloadFileInfo(et_down_url.getText().toString(),
-                Environment.getExternalStorageDirectory().getAbsolutePath() + "/aaa.jpq");
-        downloadFileManager = new DownloadFileManager(downloadFileInfo, new AbsDownloadRequestCallback() {
-            @Override
-            public void onSuccess(final Response<String> response) {
-                super.onSuccess(response);
-                runOnUiThread(new Runnable() {
-                    @Override
-                    public void run() {
-                    }
-                });
-            }
+	private void downPart() {
+		if (downloadFileManager != null && downloadFileManager.getStatus() == DownloadFileInfo.DOWNLOADING) {
+			return;
+		}
 
-            @Override
-            public void onStart() {
-                super.onStart();
-            }
+		DownloadFileInfo downloadFileInfo = new DownloadFileInfo(et_down_url.getText().toString(),
+				Environment.getExternalStorageDirectory().getAbsolutePath() + "/aaa.jpq");
+		downloadFileManager = new DownloadFileManager(downloadFileInfo, new AbsDownloadRequestCallback() {
+			@Override
+			public void onSuccess(final Response<String> response) {
+				super.onSuccess(response);
+				runOnUiThread(new Runnable() {
+					@Override
+					public void run() {
+					}
+				});
+			}
 
-            @Override
-            public void onFailure(final Throwable e) {
-                super.onFailure(e);
-                runOnUiThread(new Runnable() {
-                    @Override
-                    public void run() {
-                        error.setText(e.toString());
-                    }
-                });
-            }
+			@Override
+			public void onStart() {
+				super.onStart();
+			}
 
-            @Override
-            public void onStop(long contentLength, long bytesRead, final DownloadFileInfo fileInfo) {
-                runOnUiThread(new Runnable() {
-                    @Override
-                    public void run() {
-                        progress.setProgress((int) (fileInfo.currentFinished * 1.0f / fileInfo.fileSize * 100));
-                        progressTV.setText(fileInfo.fileSize + "/" + fileInfo.currentFinished);
-                    }
-                });
-            }
+			@Override
+			public void onFailure(final Throwable e) {
+				super.onFailure(e);
+				runOnUiThread(new Runnable() {
+					@Override
+					public void run() {
+						error.setText(e.toString());
+					}
+				});
+			}
 
-            @Override
-            public void onProgressUpdate(final long contentLength, final long bytesRead, final boolean done) {
-                super.onProgressUpdate(contentLength, bytesRead, done);
-                runOnUiThread(new Runnable() {
-                    @Override
-                    public void run() {
-                        progress.setProgress((int) (bytesRead * 1.0f / contentLength * 100));
-                        progressTV.setText(contentLength + "/" + bytesRead);
-                    }
-                });
-            }
-        });
-        downloadFileManager.startDownload();
-    }
+			@Override
+			public void onStop(long contentLength, long bytesRead, final DownloadFileInfo fileInfo) {
+				runOnUiThread(new Runnable() {
+					@Override
+					public void run() {
+						progress.setProgress((int) (fileInfo.currentFinished * 1.0f / fileInfo.fileSize * 100));
+						progressTV.setText(fileInfo.fileSize + "/" + fileInfo.currentFinished);
+					}
+				});
+			}
+
+			@Override
+			public void onProgressUpdate(final long contentLength, final long bytesRead, final boolean done) {
+				super.onProgressUpdate(contentLength, bytesRead, done);
+				runOnUiThread(new Runnable() {
+					@Override
+					public void run() {
+						progress.setProgress((int) (bytesRead * 1.0f / contentLength * 100));
+						progressTV.setText(contentLength + "/" + bytesRead);
+					}
+				});
+			}
+		});
+		downloadFileManager.startDownload();
+	}
 }
--- basetnet/src/main/java/lib/basenet/okhttp/DownloadFileManager.java
+++ basetnet/src/main/java/lib/basenet/okhttp/DownloadFileManager.java
@@ -6,185 +6,239 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.RandomAccessFile;
 import java.util.HashMap;
+import java.util.Map;
 
 import lib.basenet.NetUtils;
 import lib.basenet.request.AbsDownloadRequestCallback;
 import lib.basenet.utils.FileUtils;
+import okhttp3.CacheControl;
 import okhttp3.Call;
 import okhttp3.Callback;
+import okhttp3.Headers;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
 
 /**
+ * 注意：
+ * okhttp3.6版本 通过get下载时，请设置缓存级别为：builder.cacheControl(new CacheControl.Builder().noCache().noStore().build());
+ * 否则断点下载失效
  * Created by liyu20 on 2018/2/27.
  */
 public final class DownloadFileManager {
-    private final OkHttpClient okHttpClient;
-    private final AbsDownloadRequestCallback downloadListener;
-    private final DownloadFileInfo downloadFileInfo;
+	private final OkHttpClient okHttpClient;
+	private final AbsDownloadRequestCallback downloadListener;
+	private final DownloadFileInfo downloadFileInfo;
 
-    public DownloadFileManager(DownloadFileInfo fileInfo, AbsDownloadRequestCallback downloadListener) {
-        if(fileInfo == null || fileInfo.fileUrl == null || fileInfo.localFilePath == null) {
-            throw new RuntimeException("fileInfo or fileUrl or localFilePath  cannot be a null");
-        }
-        this.okHttpClient = NetUtils.getInstance().getOkHttpClient();
-        this.downloadListener = downloadListener;
-        this.downloadFileInfo = fileInfo;
-    }
+	public DownloadFileManager(DownloadFileInfo fileInfo, AbsDownloadRequestCallback downloadListener) {
+		if (fileInfo == null || fileInfo.fileUrl == null || fileInfo.localFilePath == null) {
+			throw new RuntimeException("fileInfo or fileUrl or localFilePath  cannot be a null");
+		}
+		this.okHttpClient = NetUtils.getInstance().getOkHttpClient();
+		this.downloadListener = downloadListener;
+		this.downloadFileInfo = fileInfo;
+		// 是否有信息
+		DownloadFileInfo cache = DownFileUtil.getCacheFileInfo(fileInfo);
+		// 已完成，重新新建DownLoadFileManger 时，删除缓存信息
+		if (cache != null && cache.status == DownloadFileInfo.SUCCESS) {
+			DownFileUtil.remove(fileInfo);
+		}
+	}
 
-    public DownloadFileManager(String downUrl, String localFileFullPath, AbsDownloadRequestCallback downloadListener) {
-        this(new DownloadFileInfo(downUrl, localFileFullPath), downloadListener);
-    }
+	public DownloadFileManager(String downUrl, String localFileFullPath, AbsDownloadRequestCallback downloadListener) {
+		this(new DownloadFileInfo(downUrl, localFileFullPath), downloadListener);
+	}
 
-    public int getStatus() {
-        return downloadFileInfo.status;
-    }
+	public int getStatus() {
+		return downloadFileInfo.status;
+	}
 
-    /**
-     * 移除下载任务
-     */
-    public void deleteDownload() {
-        NetUtils.getInstance().cancel(this.toString()); // 移除任務
-        DownFileUtil.remove(downloadFileInfo);
-        downloadFileInfo.reset();   // 清0
-        FileUtils.deleteFile(downloadFileInfo.localFilePath);
-    }
+	/**
+	 * 移除下载任务
+	 */
+	public void deleteDownload() {
+		NetUtils.getInstance().cancel(this.toString()); // 移除任務
+		DownFileUtil.remove(downloadFileInfo);
+		downloadFileInfo.reset();   // 清0
+		FileUtils.deleteFile(downloadFileInfo.localFilePath);
+	}
 
-    /**
-     * 停止下载任务
-     *
-     */
-    public void stopDownload() {
-        downloadFileInfo.status = DownloadFileInfo.DOWNLOAD_PAUSE;
-        DownFileUtil.addOrUpdate(downloadFileInfo);
-    }
+	/**
+	 * 停止下载任务
+	 */
+	public void stopDownload() {
+		NetUtils.getInstance().cancel(this.toString()); // 移除任務
+		// 完成时，不操作
+		if (downloadFileInfo.status != DownloadFileInfo.SUCCESS) {
+			downloadFileInfo.status = DownloadFileInfo.DOWNLOAD_PAUSE;
+			DownFileUtil.addOrUpdate(downloadFileInfo);
+		}
+	}
 
-    /**
-     * 开始下载
-     */
-    public void startDownload() {
-        DownloadFileInfo fileInfo = downloadFileInfo;
-        // 0. 当前正在下载，return
-        if (downloadFileInfo.status == DownloadFileInfo.DOWNLOADING) {
-            return;
-        }
-        // 1. 是否有断点信息
-        final DownloadFileInfo cacheFileInfo = DownFileUtil.getCacheFileInfo(fileInfo);
+	/**
+	 * 开始下载
+	 */
+	public void startDownload() {
+		// 0. 当前正在下载，return
+		if (downloadFileInfo.status == DownloadFileInfo.DOWNLOADING) {
+			return;
+		}
+		// 1. 是否有断点信息
+		final DownloadFileInfo cacheFileInfo = DownFileUtil.getCacheFileInfo(downloadFileInfo);
 
-        if (cacheFileInfo != null) {  // 賦值進度
-            fileInfo.currentFinished = cacheFileInfo.currentFinished;
-            fileInfo.fileSize = cacheFileInfo.fileSize;
-        }
-        DownFileUtil.addOrUpdate(fileInfo);
-        downloadFile(fileInfo);
-    }
+		if (cacheFileInfo != null) {  // 賦值進度
+			downloadFileInfo.currentFinished = cacheFileInfo.currentFinished;
+			downloadFileInfo.fileSize = cacheFileInfo.fileSize;
+			downloadFileInfo.status = cacheFileInfo.status;
+		}
 
-    private void downloadFile(final DownloadFileInfo fileInfo) {
-        //已有下载信息，则添加Header，若无则正常请求
-        Request.Builder builder = new Request.Builder();
-        builder.tag(this.toString());
-        final File file = new File(fileInfo.localFilePath);
-        // 校验文件
-        switch (fileInfo.status) {
-            case DownloadFileInfo.SUCCESS:       //已下载完成
-                return;
-            case DownloadFileInfo.DOWNLOADING:   //正在下载
-                return;
-            case DownloadFileInfo.FAILURE:
-            case DownloadFileInfo.DOWNLOAD_PAUSE:
-                if (file.exists()) {    // 断点下载
-                    builder.addHeader("Range", "bytes=" + fileInfo.currentFinished + "-" + fileInfo.fileSize);
-                }
-                break;      // break
-            case DownloadFileInfo.NOT_BEGIN:
-                if (file.exists()) {
-                    file.delete();
-                }
-                break;      // break
-        }
-        Request request = builder.url(fileInfo.fileUrl).build();
-        downloadRequest(request, fileInfo, file);
-    }
+		DownFileUtil.addOrUpdate(downloadFileInfo);
+		downloadFile(downloadFileInfo);
+	}
 
-    private void downloadRequest(Request request, final DownloadFileInfo fileInfo, final File file) {
-        if (downloadListener != null) {
-            downloadListener.onStart();
-        }
+	private void downloadFile(final DownloadFileInfo fileInfo) {
+		//已有下载信息，则添加Header，若无则正常请求
+		Request.Builder builder = new Request.Builder();
+		builder.tag(this.toString());
+		final File file = new File(fileInfo.localFilePath);
+		// 校验文件
+		switch (fileInfo.status) {
+			case DownloadFileInfo.SUCCESS:       //已下载完成
+				return;
+			case DownloadFileInfo.DOWNLOADING:   //正在下载
+				return;
+			case DownloadFileInfo.FAILURE:
+			case DownloadFileInfo.DOWNLOAD_PAUSE:
+				if (file.exists()) {    // 断点下载
+					builder.addHeader("Range", "bytes=" + fileInfo.currentFinished + "-");
+				}
+				break;      // break
+			case DownloadFileInfo.NOT_BEGIN:
+				if (file.exists()) {
+					file.delete();
+					fileInfo.reset();    // 复位
+				}
+				break;      // break
+		}
+		// 下载，必须要加cacheControl控制
+		builder.cacheControl(new CacheControl.Builder().noCache().noStore().build());
+		Request request = builder.url(fileInfo.fileUrl).build();
+		downloadRequest(request, fileInfo, file);
+	}
 
-        //发起请求
-        okHttpClient.newCall(request).enqueue(new Callback() {
-            @Override
-            public void onFailure(Call call, IOException e) {
-                fileInfo.status = DownloadFileInfo.FAILURE;
-                DownFileUtil.addOrUpdate(fileInfo);
-                if (downloadListener != null) {
-                    downloadListener.onFailure(e);
-                }
-            }
+	private void downloadRequest(Request request, final DownloadFileInfo fileInfo, final File file) {
+		if (downloadListener != null) {
+			downloadListener.onStart();
+		}
 
-            @Override
-            public void onResponse(Call call, Response response) throws IOException {
-                if (!response.isSuccessful()) {
-                    //下载失败
-                    fileInfo.status = DownloadFileInfo.FAILURE;
-                    if (downloadListener != null) {
-                        downloadListener.onFailure(new Exception(response.code() + " " + response.message()));
-                    }
-                    return;
-                }
+		//发起请求
+		okHttpClient.newCall(request).enqueue(new Callback() {
+			@Override
+			public void onFailure(Call call, IOException e) {
+				fileInfo.status = DownloadFileInfo.FAILURE;
+				DownFileUtil.addOrUpdate(fileInfo);
+				if (downloadListener != null) {
+					if(downloadListener != null && call.isCanceled()) {
+						downloadListener.onCancel();
+					} else {
+						downloadListener.onFailure(e);
+					}
+				}
+			}
 
-                InputStream is;
-                byte[] buffer = new byte[4096];
-                int len;
-                try {
-                    is = response.body().byteStream();
-                    long total = response.body().contentLength();
-                    //不设置断点下载时，文件大小才是真实大小
-                    if (fileInfo.status == DownloadFileInfo.NOT_BEGIN) {
-                        fileInfo.fileSize = total;
-                    }
-                    if (!file.exists()) {
-                        file.createNewFile();
-                    }
-                    //设置状态值
-                    fileInfo.status = DownloadFileInfo.DOWNLOADING;
-                    RandomAccessFile randomFile = new RandomAccessFile(file, "rwd");
-                    randomFile.seek(fileInfo.currentFinished);
-                    //http 206部分下载，感觉不需要
-                    while ((len = is.read(buffer)) != -1) {
-                        //写入文件`
-                        randomFile.write(buffer, 0, len);
-                        fileInfo.currentFinished += len;
-                        if (downloadListener != null) {
-                            downloadListener.onProgressUpdate(fileInfo.fileSize, fileInfo.currentFinished, fileInfo.fileSize == fileInfo.currentFinished);
-                        }
-                        //停止下载
-                        if (fileInfo.status != DownloadFileInfo.DOWNLOADING) {
-                            randomFile.close();
-                            if (downloadListener != null) {
-                                downloadListener.onStop(fileInfo.fileSize, fileInfo.currentFinished, fileInfo);
-                            }
-                            return;
-                        }
-                    }
+			@Override
+			public void onResponse(Call call, Response response) throws IOException {
+				if (!response.isSuccessful()) {
+					//下载失败
+					fileInfo.status = DownloadFileInfo.FAILURE;
+					if (downloadListener != null) {
+						downloadListener.onFailure(new Exception(response.code() + " " + response.message()));
+					}
+					return;
+				}
 
-                    //下载完成
-                    randomFile.close();
-                    fileInfo.status = DownloadFileInfo.SUCCESS;
-                    if (downloadListener != null) {
-                        final lib.basenet.response.Response myResponse = new lib.basenet.response.Response(null, new HashMap<String, String>(), fileInfo.localFilePath);
-                        myResponse.statusCode = response.code();
-                        downloadListener.onSuccess(myResponse);
-                    }
-                } catch (Exception e) {
-                    DownFileUtil.addOrUpdate(fileInfo); // 更新信息
-                    e.printStackTrace();
-                    if (downloadListener != null) {
-                        downloadListener.onFailure(new Exception(response.code() + " " + response.message()));
-                    }
-                }
-            }
-        });
-    }
+				// 影响Header
+				Map<String, String> respHeader = getResponseHeaders(response);
+
+				InputStream is;
+				byte[] buffer = new byte[4096];
+				int len;
+				try {
+					is = response.body().byteStream();
+					long total = response.body().contentLength();
+					//不设置断点下载时，文件大小才是真实大小
+					if (fileInfo.status == DownloadFileInfo.NOT_BEGIN) {
+						fileInfo.fileSize = total;
+					}
+					if (!file.exists()) {
+						file.createNewFile();
+					}
+					//设置状态值
+					fileInfo.status = DownloadFileInfo.DOWNLOADING;
+					RandomAccessFile randomFile = new RandomAccessFile(file, "rwd");
+					// 断点下载，服务端返回206
+					if (response.code() != 206) {
+						fileInfo.currentFinished = 0;    // 重新下载
+					}
+					randomFile.seek(fileInfo.currentFinished);
+					while ((len = is.read(buffer)) != -1) {
+						//写入文件`
+						randomFile.write(buffer, 0, len);
+						fileInfo.currentFinished += len;
+						if (downloadListener != null) {
+							downloadListener.onProgressUpdate(fileInfo.fileSize, fileInfo.currentFinished, fileInfo.fileSize == fileInfo.currentFinished);
+						}
+						//停止下载
+						if (fileInfo.status != DownloadFileInfo.DOWNLOADING) {
+							randomFile.close();
+							if (downloadListener != null) {
+								downloadListener.onStop(fileInfo.fileSize, fileInfo.currentFinished, fileInfo);
+							}
+							return;
+						}
+					}
+
+					//下载完成
+					randomFile.close();
+					fileInfo.status = DownloadFileInfo.SUCCESS;
+					DownFileUtil.addOrUpdate(fileInfo);        // 更新
+					if (downloadListener != null) {
+						final lib.basenet.response.Response myResponse =
+								new lib.basenet.response.Response(null, respHeader, fileInfo.localFilePath);
+						myResponse.statusCode = response.code();
+						downloadListener.onSuccess(myResponse);
+					}
+				} catch (Exception e) {
+					DownFileUtil.addOrUpdate(fileInfo); // 更新信息
+
+					if (downloadListener != null) {
+						if(downloadListener != null && call.isCanceled()) {
+							downloadListener.onCancel();
+						} else {
+							downloadListener.onFailure(new Exception(response.code() + " " + response.message()));
+						}
+					}
+				}
+			}
+		});
+	}
+
+	/**
+	 * 封装响应 header
+	 *
+	 * @param response
+	 * @return
+	 */
+	private HashMap getResponseHeaders(Response response) {
+		HashMap headerMap = null;
+		if (null != response.headers() && response.headers().size() > 0) {
+			headerMap = new HashMap<>();
+			Headers responseHeaders = response.headers();
+			for (int i = 0; i < responseHeaders.size(); i++) {
+				headerMap.put(responseHeaders.name(i), responseHeaders.value(i));
+			}
+		}
+
+		return headerMap;
+	}
 }
